3f36a4dd3866f345719622677c5e60ebe263a3a9
FK <fkinolli@pinewood-school.gr>
2020-08-26 13:15:01 +0300

    more testing

--- Customer Service Bot/databaseTest.py
+++ Customer Service Bot/databaseTest.py
@@ -13,7 +13,68 @@ mycursor = mydb.cursor()
 
 
 
-# For printing out all db's
+
+
+
+
+#-------------------------------------------------------------------------------
+
+##https://www.youtube.com/watch?v=jA1GO6g_Rw0
+##--++ Selecting and Getting Data ++--
+##selects specific column to be displayed
+#mycursor.execute("SELECT name FROM users")
+
+##selects all columns to be displayed
+#mycursor.execute("SELECT * FROM users")
+
+##fetches all entrys
+#myresult = mycursor.fetchall()
+
+##fetches one entry, usually first row
+#myresult = mycursor.fetchone()
+
+##printing fetched data
+#for row in myresult:
+#    print(row)
+
+#-------------------------------------------------------------------------------
+
+##https://www.youtube.com/watch?v=BfXhZDNlXy8
+##--++ Template for automatic data insertion through inputted variables
+##takes in user credentials
+#name = input("Name: ")
+#password = input("Password: ")
+#email = input("Email: ")
+
+##is the command for inputting data into database
+#mycursor = mydb.cursor()
+#sqlformula = "INSERT INTO users (name, password, email) VALUES (%s, %s, %s)"
+#user = (name, password, email)
+
+##executes both sqlformula and user in order to send data to database
+#mycursor.execute(sqlformula, user)
+
+##needed to finalize entry, similar to commiting on github,etc.
+#mydb.commit()
+
+#-------------------------------------------------------------------------------
+
+##https://www.youtube.com/watch?v=BfXhZDNlXy8
+##--++ Template for inserting data ++--
+#sqlformula = "INSERT INTO users (name, password, email) VALUES (%s, %s, %s)"
+
+##manualling inserting data, needs to be replaced with variables user inputs
+#user1 = ("Jeff", "jeff2002", "jeff@gmail.com")
+
+##executes both sqlformula and user1 in order to send data to database
+#mycursor.execute(sqlformula, user1)
+
+##needed to finalize entry, similar to commiting on github,etc.
+#mydb.commit()
+
+#-------------------------------------------------------------------------------
+
+##--++ For printing out all db's ++--
 #for db in mycursor:
 #    print(db)
 
