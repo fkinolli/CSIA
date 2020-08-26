0bdee7fafa3819afb714e900677eb860ee12b233
FK <fkinolli@pinewood-school.gr>
2020-08-26 13:32:47 +0300

    more testing

--- Customer Service Bot/databaseTest.py
+++ Customer Service Bot/databaseTest.py
@@ -19,8 +19,38 @@ mycursor = mydb.cursor()
 
 #-------------------------------------------------------------------------------
 
+##https://www.youtube.com/watch?v=OTzL0oH-ZGI
+##--++ Query Conditions with WHERE and Wildcards ++--
+
+##Searching with conditions
+#sql = "SELECT * FROM users WHERE name ='Fabio'"
+#mycursor.execute(sql)
+#myresult = mycursor.fetchall()
+#
+#for result in myresult:
+#    print(result)
+
+##Searaching with wildcards where the % sign is the wild card, in this case anything beginning with F would be displayed.
+#sql = "SELECT * FROM users WHERE name LIKE 'F%'"
+#mycursor.execute(sql)
+#myresult = mycursor.fetchall()
+
+#for result in myresult:
+#    print(result)
+
+##Always good to use placeholders("%s") and then passing in values as this is safer and more secure from sqlInjection attacks, need to be passed through a little different as seen in the execute command below.
+#sql = "SELECT * FROM users WHERE name LIKE %s"
+#mycursor.execute(sql, ('F%', ))
+#myresult = mycursor.fetchall()
+#
+#for result in myresult:
+#    print(result)
+
+#-------------------------------------------------------------------------------
+
 ##https://www.youtube.com/watch?v=jA1GO6g_Rw0
 ##--++ Selecting and Getting Data ++--
+
 ##selects specific column to be displayed
 #mycursor.execute("SELECT name FROM users")
 
@@ -40,7 +70,8 @@ mycursor = mydb.cursor()
 #-------------------------------------------------------------------------------
 
 ##https://www.youtube.com/watch?v=BfXhZDNlXy8
-##--++ Template for automatic data insertion through inputted variables
+##--++ Template for automatic data insertion through inputted variables ++--
+
 ##takes in user credentials
 #name = input("Name: ")
 #password = input("Password: ")
@@ -61,6 +92,7 @@ mycursor = mydb.cursor()
 
 ##https://www.youtube.com/watch?v=BfXhZDNlXy8
 ##--++ Template for inserting data ++--
+
 #sqlformula = "INSERT INTO users (name, password, email) VALUES (%s, %s, %s)"
 
 ##manualling inserting data, needs to be replaced with variables user inputs
